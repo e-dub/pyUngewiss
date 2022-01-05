e5f0ded777e7149f45928490be029dcf9cd4cdfb
wehrle <Erich.Wehrle@unibz.it>
2022-01-05 20:53:26 +0100

    readme

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from distutils.core import setup
 import os
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
 
 
 def package_files(directory):
@@ -14,11 +18,13 @@ if __name__ == '__main__':
     extra_files = package_files('pyUngewiss')
     setup(
         name='pyUngewiss',
-        version='1.0',
+        version='1.1',
         author='E. J. Wehrle',
         author_email='Erich.Wehrle@unibz.it',
         packages=['pyUngewiss'],
         package_data={'': extra_files},
         description='Python library for UNcertainty analysis in liGhtwEight dsiGn with IntervalS and fuzzy numberS',
         license='GPL3',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
     )
